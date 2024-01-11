87eebbb28e190bdd5fd5a0cf1d35fda457da7ad6
FunnyAWM <1142978300@qq.com>
2024-01-11 15:18:15 +0800

    Test code written.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include "game.h"
 #include "func.h"
+#include <ctime>
 
 using std::cout;
 using std::cin;
@@ -7,7 +8,12 @@ using std::endl;
 using std::getline;
 
 int main() {
+    clock_t start,end;
+    start=clock();
     gameInit();
+    end=clock();
+    auto time=double(end-start)/CLOCKS_PER_SEC;
+    cout << endl << time << " secs in total.";
     getchar();
     closeHandle();
 }
