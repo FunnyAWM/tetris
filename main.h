00ba2cdc205a98418e68bdf0ab414d4de6016f03
FunnyAWM <1142978300@qq.com>
2024-01-11 10:28:39 +0800

    Header function defined.

--- main.h
+++ main.h
@@ -5,7 +5,19 @@
 #include <iostream>
 #include <ctime>
 
-//系统调用模块
+namespace TETRIS{
+    //系统调用模块
+    void initHandle(){
+        //TODO
+    }
 
+    void setColor(int color){
+        //TODO
+    }
 
+    void setCursorVisibility(){
+        //TODO
+    }
+
+}
 #endif //TETRIS_MAIN_H
